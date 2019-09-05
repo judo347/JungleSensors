68e628dd5b33b7064efbe7ae38b37ffec325d5ce
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2019-09-05 10:42:30 +0200

    Basic libgdx setup

---
+++ game/desktop/src/dk/mk/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dk.mk.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dk.mk.game.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
